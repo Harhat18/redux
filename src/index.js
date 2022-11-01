167723e74aa412cde2c58416e9cbc23b63fd7392
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-11-01 20:33:38 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+function reducer() {
+  return "state";
+}
+const store = createStore(reducer);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(store.getState());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
